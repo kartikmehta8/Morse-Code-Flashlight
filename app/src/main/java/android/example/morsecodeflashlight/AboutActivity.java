88a610b4d911b656894ebb870cda9ba4a7a77b47
TylerCYan <90355620+TylerCYan@users.noreply.github.com>
2022-05-29 23:50:25 -0400

    Updated About page (draft)

--- app/src/main/java/android/example/morsecodeflashlight/AboutActivity.java
+++ app/src/main/java/android/example/morsecodeflashlight/AboutActivity.java
@@ -7,6 +7,7 @@ public class AboutActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTitle("About");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
     }
